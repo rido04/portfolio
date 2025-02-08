bcd34156009ae39035a4dde896be9bc956356c94
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-02-08 13:55:56 +0700

    fix js untuk tombol hamburger

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
 
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  menu.classList.toggle("flex");
-  menu.classList.toggle("flex-col");
-  menu.classList.toggle("absolute");
-  menu.classList.toggle("top-16");
-  menu.classList.toggle("left-0");
-  menu.classList.toggle("bg-white");
-  menu.classList.toggle("w-full");
-  menu.classList.toggle("shadow-lg");
-  menu.classList.toggle("p-4");
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("flex");
+    menu.classList.toggle("flex-col");
+    menu.classList.toggle("absolute");
+    menu.classList.toggle("top-16");
+    menu.classList.toggle("left-0");
+    menu.classList.toggle("bg-white");
+    menu.classList.toggle("w-full");
+    menu.classList.toggle("shadow-lg");
+    menu.classList.toggle("p-4");
+  });
 });
 
 const themeToggle = document.getElementById("theme-toggle");
